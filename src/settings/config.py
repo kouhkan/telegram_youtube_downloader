1ede7b7a7ae2250de2f3d7867eb8c3aabca15252
kouhkan <amirkouhkan1@gmail.com>
2024-03-12 20:29:33 +0330

    feat: change env names

--- src/settings/config.py
+++ src/settings/config.py
@@ -7,11 +7,11 @@ load_dotenv()
 settings = dict()
 settings["TELEGRAM_TOKEN"] = os.getenv("TELEGRAM_TOKEN")
 settings["DB_URI"] = "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
-    os.getenv('DB_USER'),
-    os.getenv('DB_PASS'),
-    os.getenv('DB_HOST'),
-    os.getenv('DB_PORT'),
-    os.getenv('DB_DB')
+    os.getenv('POSTGRES_USER'),
+    os.getenv('POSTGRES_PASSWORD'),
+    os.getenv('POSTGRES_HOST'),
+    os.getenv('POSTGRES_PORT'),
+    os.getenv('POSTGRES_DB')
 )
 settings["CELERY_BROKER_URI"] = os.getenv("CELERY_BROKER_URI")
 settings["CELERY_BACKEND"] = os.getenv("CELERY_BACKEND")
