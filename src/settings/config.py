f2f99cea3b3b3f4ed866e2fff74e58c6fae860da
kouhkan <amirkouhkan1@gmail.com>
2024-03-12 16:06:04 +0330

    feat: celery settings

--- src/settings/config.py
+++ src/settings/config.py
@@ -13,3 +13,5 @@ settings["DB_URI"] = "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
     os.getenv('DB_PORT'),
     os.getenv('DB_DB')
 )
+settings["CELERY_BROKER_URI"] = os.getenv("CELERY_BROKER_URI")
+settings["CELERY_BACKEND"] = os.getenv("CELERY_BACKEND")
